5a8750550640fb300688b205e2f16391a5841cb9
Chiheb Ben Hammouda <chiheb.benhammouda@kaust.edu.sa>
2018-09-24 19:36:28 +0300

    Bergomi_code_tex_changes

--- Dropbox/Smoothing_project/code/mimclib/tests/Bergomi_using_C+/plotting_complexity_MISC_MC.py
+++ Dropbox/Smoothing_project/code/mimclib/tests/Bergomi_using_C+/plotting_complexity_MISC_MC.py
@@ -79,27 +79,27 @@ from matplotlib.ticker import MaxNLocator
 # fit_MC=np.exp(z_MC[0]*np.log(MC_err))
 # print z_MC[0]
 
-# MISC_err=np.array([0.5159,0.2934,0.1558])
-# MISC_time=np.array([ 0.2,11,5760])
+MISC_err=np.array([0.5159,0.2934,0.1558])
+MISC_time=np.array([ 0.2,11,5760])
 
-# z_MISC= np.polyfit(np.log(MISC_err), np.log(MISC_time), 1)
-# fit_MISC=np.exp(z_MISC[0]*np.log(MISC_err))
-# print z_MISC[0]
+z_MISC= np.polyfit(np.log(MISC_err), np.log(MISC_time), 1)
+fit_MISC=np.exp(z_MISC[0]*np.log(MISC_err))
+print z_MISC[0]
 
 # # # #Case for parameter set 1, with rich
-# # MC_rich_err=np.array([ 0.7561,0.0715,0.0141 ])
-# # MC_rich_time=np.array([  34.7,37,532])
+MC_rich_err=np.array([ 0.7561,0.0758,0.0141 ])
+MC_rich_time=np.array([  34.7,37,532])
 
-# # z_MC_rich= np.polyfit(np.log(MC_rich_err), np.log(MC_rich_time), 1)
-# # fit_MC_rich=np.exp(z_MC_rich[0]*np.log(MC_rich_err))
-# # print z_MC_rich[0]
+z_MC_rich= np.polyfit(np.log(MC_rich_err), np.log(MC_rich_time), 1)
+fit_MC_rich=np.exp(z_MC_rich[0]*np.log(MC_rich_err))
+print z_MC_rich[0]
 
-# MISC_rich_err=np.array([0.7561,0.0715,0.0141])
-# MISC_rich_time=np.array([ 4,191,664])
+MISC_rich_err=np.array([0.7561,0.0758,0.0141])
+MISC_rich_time=np.array([ 4,12,520])
 
-# z_MISC_rich= np.polyfit(np.log(MISC_rich_err), np.log(MISC_rich_time), 1)
-# fit_MISC_rich=np.exp(z_MISC_rich[0]*np.log(MISC_rich_err))
-# print z_MISC_rich[0]
+z_MISC_rich= np.polyfit(np.log(MISC_rich_err), np.log(MISC_rich_time), 1)
+fit_MISC_rich=np.exp(z_MISC_rich[0]*np.log(MISC_rich_err))
+print z_MISC_rich[0]
 ##############################################################################
 
 
@@ -208,21 +208,21 @@ from matplotlib.ticker import MaxNLocator
 
 # # #Case for parameter set 3, non rich
 
-MC_err=np.array([0.1118])       	
+# MC_err=np.array([0.1118])       	
 	
-MC_time=np.array([ 188])  
+# MC_time=np.array([ 188])  
 		
 
-z_MC= np.polyfit(np.log(MC_err), np.log(MC_time), 1)
-fit_MC=np.exp(z_MC[0]*np.log(MC_err))
-print z_MC[0]
+# z_MC= np.polyfit(np.log(MC_err), np.log(MC_time), 1)
+# fit_MC=np.exp(z_MC[0]*np.log(MC_err))
+# print z_MC[0]
 
-MISC_err=np.array([0.1119,])
-MISC_time=np.array([ 25])
+# MISC_err=np.array([0.1119,])
+# MISC_time=np.array([ 25])
 
-z_MISC= np.polyfit(np.log(MISC_err), np.log(MISC_time), 1)
-fit_MISC=np.exp(z_MISC[0]*np.log(MISC_err))
-print z_MISC[0]
+# z_MISC= np.polyfit(np.log(MISC_err), np.log(MISC_time), 1)
+# fit_MISC=np.exp(z_MISC[0]*np.log(MISC_err))
+# print z_MISC[0]
 
 
 ##############################################################################
@@ -230,8 +230,8 @@ print z_MISC[0]
 
 fig = plt.figure()
 
-plt.plot(MC_err,MC_time,linewidth=2.0,label='MC' , marker='>',hold=True) 
-plt.plot(MC_err, fit_MC*1000,linewidth=2.0,label=r'rate= %s' % format(z_MC[0]  , '.2f'), linestyle = '--')
+# plt.plot(MC_err,MC_time,linewidth=2.0,label='MC' , marker='>',hold=True) 
+# plt.plot(MC_err, fit_MC*1000,linewidth=2.0,label=r'rate= %s' % format(z_MC[0]  , '.2f'), linestyle = '--')
 
 plt.plot(MISC_err,MISC_time,linewidth=2.0,label='MISC'  , marker='v',hold=True) 
 plt.plot(MISC_err, fit_MISC*10,linewidth=2.0,label=r'rate= %s' % format(z_MISC[0]  , '.2f'), linestyle = '--')
@@ -239,8 +239,8 @@ plt.plot(MISC_err, fit_MISC*10,linewidth=2.0,label=r'rate= %s' % format(z_MISC[0
 # plt.plot(MC_rich_err,MC_rich_time,linewidth=2.0,label='MC+Rich' , marker='>',hold=True) 
 # plt.plot(MC_rich_err, fit_MC_rich*10,linewidth=2.0,label=r'rate= %s' % format(z_MC_rich[0]  , '.2f'), linestyle = '--')
 
-# plt.plot(MISC_rich_err,MISC_rich_time,linewidth=2.0,label='MISC+Rich'  , marker='v',hold=True) 
-# plt.plot(MISC_rich_err, fit_MISC_rich*10,linewidth=2.0,label=r'rate= %s' % format(z_MISC_rich[0]  , '.2f'), linestyle = '--')
+plt.plot(MISC_rich_err,MISC_rich_time,linewidth=2.0,label='MISC+Rich'  , marker='v',hold=True) 
+plt.plot(MISC_rich_err, fit_MISC_rich*10,linewidth=2.0,label=r'rate= %s' % format(z_MISC_rich[0]  , '.2f'), linestyle = '--')
 
 plt.yscale('log')
 plt.xscale('log')
@@ -249,4 +249,4 @@ plt.xlabel('Error',fontsize=14)
 plt.ylabel('CPU time',fontsize=14) 
 plt.subplots_adjust(wspace=0.6, hspace=0.6, left=0.15, bottom=0.22, right=0.96, top=0.96)
 plt.legend(loc='upper right')
-plt.savefig('./results/error_vs_time_set3.eps', format='eps', dpi=1000)  
+plt.savefig('./results/error_vs_time_set1_comparison.eps', format='eps', dpi=1000)  
--- Dropbox/Smoothing_project/code/mimclib/tests/Bergomi_using_C+/testing_weak_convergence_richardson_MC_parallel.py
+++ Dropbox/Smoothing_project/code/mimclib/tests/Bergomi_using_C+/testing_weak_convergence_richardson_MC_parallel.py
@@ -177,7 +177,7 @@ def weak_convergence_differences():
         Lb=np.zeros(4)
         Ub_diff=np.zeros(3)
         Lb_diff=np.zeros(3)
-        values=np.zeros((2*(10**6),4)) 
+        values=np.zeros((4*(10**6),4)) 
      
         num_cores = mp.cpu_count()
         print num_cores
@@ -199,7 +199,7 @@ def weak_convergence_differences():
             # results = Parallel(n_jobs=num_cores)(delayed(processInput)(j) for j in inputs)
             p =  pp.ProcessPool(num_cores)  # Processing Pool with four processors
             
-            values[:,i]= p.map(processInput, range((2*(10**6))))
+            values[:,i]= p.map(processInput, range((4*(10**6))))
 
             elapsed_time_qoi[i]=time.time()-start_time
             print  elapsed_time_qoi[i]
@@ -210,7 +210,7 @@ def weak_convergence_differences():
         error=np.abs(np.mean(values,axis=0) - 1) 
         elapsed_time_qoi=time.time()-start_time_2+elapsed_time_qoi
 
-        stand=np.std(values, axis = 0)/  float(np.sqrt((2*(10**6))))
+        stand=np.std(values, axis = 0)/  float(np.sqrt((4*(10**6))))
         Ub=np.abs(np.mean(values,axis=0) - 1)+1.96*stand
         Lb=np.abs(np.mean(values,axis=0) - 1)-1.96*stand
 
@@ -224,7 +224,7 @@ def weak_convergence_differences():
         differences= [values[:,i]-values[:,i+1] for i in range(0,3)]
         error_diff=np.abs(np.mean(differences,axis=1))
         print error_diff 
-        stand_diff=np.std(differences, axis = 1)/ float(np.sqrt((2*(10**6))))
+        stand_diff=np.std(differences, axis = 1)/ float(np.sqrt((4*(10**6))))
         print stand_diff
         Ub_diff=np.abs(np.mean(differences,axis=1))+1.96*stand_diff
         Lb_diff=np.abs(np.mean(differences,axis=1))-1.96*stand_diff
@@ -267,7 +267,7 @@ def weak_convergence_differences():
         plt.ylabel(r'$\mid  g(X_{\Delta t})-  g(X) \mid $',fontsize=14) 
         plt.subplots_adjust(wspace=0.6, hspace=0.6, left=0.15, bottom=0.22, right=0.96, top=0.96)
         plt.legend(loc='upper left')
-        plt.savefig('./results/weak_convergence_order_Bergomi_H_002_K_12_M_2_10_6_richardson_relative.eps', format='eps', dpi=1000)  
+        plt.savefig('./results/weak_convergence_order_Bergomi_H_002_K_12_M_4_10_6_richardson_relative.eps', format='eps', dpi=1000)  
 
         fig = plt.figure()
         plt.plot(dt_arr[0:3], error_diff,linewidth=2.0,label='weak_error' ,marker='>', hold=True) 
@@ -282,7 +282,7 @@ def weak_convergence_differences():
         plt.ylabel(r'$\mid  g(X_{\Delta t})-  g(X_{\Delta t/2}) \mid $',fontsize=14) 
         plt.subplots_adjust(wspace=0.6, hspace=0.6, left=0.15, bottom=0.22, right=0.96, top=0.96)
         plt.legend(loc='upper left')
-        plt.savefig('./results/weak_convergence_order_differences_Bergomi_H_002_K_12_M_2_10_6_richardson_relative.eps', format='eps', dpi=1000)  
+        plt.savefig('./results/weak_convergence_order_differences_Bergomi_H_002_K_12_M_4_10_6_richardson_relative.eps', format='eps', dpi=1000)  
 
 #weak_convergence_rate_plotting()
 weak_convergence_differences()
