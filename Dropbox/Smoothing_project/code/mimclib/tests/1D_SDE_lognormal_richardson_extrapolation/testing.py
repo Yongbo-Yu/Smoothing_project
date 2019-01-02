2082103f4fc4c2e142983d1bcb2632e31549a7c9
Chiheb Ben Hammouda <chiheb.benhammouda@kaust.edu.sa>
2019-01-02 13:10:44 +0300

    numeircal_smoothing_update

--- Dropbox/Smoothing_project/code/mimclib/tests/1D_SDE_lognormal_richardson_extrapolation/testing.py
+++
@@ -1,5 +0,0 @@
-import numpy as np 
-
-yf = np.array([1,2,3,4,5,6])
-yc=[sum(yf[current: current+2]) for current in xrange(0, len(yf), 2)]
-print(yc)
--- Dropbox/Smoothing_project/code/mimclib/tests/plot_prog.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-if __name__ == "__main__":
-    from mimclib.plot import run_program
-    run_program()
-    
